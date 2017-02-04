d1814797e97fe71cbc4cb493449e25548eef2c42
Nate Kinney <nathan.jeff.kinney@gmail.com>
2017-02-04 13:30:18 -0500

    Added popcornMotor to RobotMap and collectorMotor to RobotMap. Created PopcornMachine subsystem.

---
+++ 2017 Robot/src/org/usfirst/frc/team58/robot/subsystems/PopcornMachine.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team58.robot.subsystems;
+
+import org.usfirst.frc.team58.robot.commands.Drive;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class PopcornMachine extends Subsystem{
+	private Talon popcornTalon;
+	
+	
+	public void initDefaultCommand(){
+		//setDefaultCommand(new ());
+	}
+}
